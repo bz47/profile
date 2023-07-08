f90610f328ef7c74fb94e77b534e5448092b9c22
b-ez47 <noreply>
2023-07-08 16:44:51 -0400

    add pic

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,12 +17,14 @@ function Projects() {
         <h1>Projects</h1>
         <div className={styles.projects}>
           <div className={styles.cards}>
-            <div className={styles.img}></div>
+            <div className={styles.img}>
+              <img src="/Users/brandonreleford/Desktop/DevProjects/reactApps/profile/public/Screenshot.png" />
+            </div>
             <div className={styles.holder}>
-              <h3 className={styles.title}>Netlify App</h3>
+              <h3 className={styles.title}>Whatsapp CopyCat</h3>
               <p className={styles.desc}> This is an app for netlify</p>
               <a
-                href="https://github.com/bz47/netlifypracticeapp"
+                href="https://whatsappcopycat.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
